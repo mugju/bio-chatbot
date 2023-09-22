d27f541cde7c46b78795ce299f55aeffdf69ba0c
dongd <ddaa1155k@gmail.com>
2023-09-22 23:24:24 +0900

    deploy setting

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.routes import predictor
+# from api.routes import predictor
 
 router = APIRouter()
-router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
+# router.include_router(predictor.router, tags=["predictor"], prefix="/v1")

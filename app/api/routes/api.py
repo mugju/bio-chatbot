00e43b7218e4312c9986b4f193bacc7769edf3c1
dongd <ddaa1155k@gmail.com>
2023-11-18 23:13:31 +0900

    feature : api경로 수정

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 import chatbot
 
 router = APIRouter()
-router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
+router.include_router(chatbot.router, tags=["predictor"], prefix="/v1")
